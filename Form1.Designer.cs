a64cf4fb31863fc1ab73d7721df77bb3714d14a0
Erik Rony <erik.rony@hotmail.com>
2023-08-11 20:57:40 -0300

    programa criado para treinamento em Csharp

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace AppSimples1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.betValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultValue = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.resultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // betValue
+            // 
+            this.betValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.betValue.Location = new System.Drawing.Point(301, 71);
+            this.betValue.Name = "betValue";
+            this.betValue.Size = new System.Drawing.Size(264, 40);
+            this.betValue.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(283, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Saldo (clique em cima para exibir):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(210, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Aposta:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(301, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(264, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Aposta!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // resultValue
+            // 
+            this.resultValue.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.resultValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultValue.Location = new System.Drawing.Point(133, 323);
+            this.resultValue.Multiline = true;
+            this.resultValue.Name = "resultValue";
+            this.resultValue.ReadOnly = true;
+            this.resultValue.Size = new System.Drawing.Size(425, 74);
+            this.resultValue.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 358);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Resultado:";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(301, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 43);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Dobrar aposta";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(434, 117);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 43);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Avançar >>";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // resultado
+            // 
+            this.resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultado.Location = new System.Drawing.Point(296, 29);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(262, 30);
+            this.resultado.TabIndex = 9;
+            this.resultado.Click += new System.EventHandler(this.resultado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(584, 411);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resultValue);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.betValue);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AppSimples";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox betValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox resultValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label resultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppSimples1
+{
+    public partial class Form1 : Form
+    {
+        int apostaValue;
+        int apostaResultado;
+        int saldoBanca = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int betIntValueDouble = Convert.ToInt32(betValue.Text);
+            betIntValueDouble = betIntValueDouble * 2;
+            betValue.Text = betIntValueDouble.ToString();
+            
+        }
+
+        private void saldoValue_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void resultado_Click(object sender, EventArgs e)
+        {
+            resultado.Text = saldoBanca.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (saldoBanca > 1) {
+                int betIntValue = Convert.ToInt32(betValue.Text);
+                saldoBanca = saldoBanca - betIntValue;
+                resultValue.Text = ("Oops! Você perdeu $ " + betIntValue + "seu saldo é: $" + saldoBanca.ToString());
+                resultado.Text = saldoBanca.ToString();
+
+            }
+            else
+            {
+                resultValue.Text = "Não há saldo suficiente!";
+            }
+           
+
+        }
+
+    }
+}
